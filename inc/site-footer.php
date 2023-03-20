da833bc782b15fa7cf8f37508a9cd801c85ca6e6
Kasutan <contact@kasutan.pro>
2023-03-20 20:42:24 +0100

    Contenu footer Style footer mobile (à suivre)

--- inc/site-footer.php
+++ inc/site-footer.php
@@ -1,54 +1,122 @@
 <?php
 
+/**
+* Logo + Formulaire newsletter
+*
+*/
+add_action( 'tha_footer_top', 'kasutan_footer_top' );
+function kasutan_footer_top() {
+	$logo=$form='';
+	if(function_exists('get_field')) {
+		$logo=esc_attr(get_field('ohm_logo_footer','option'));
+		$form=wp_kses_post(get_field('ohm_newsletter','option'));
+	}
+	if(!$logo && !$form) {
+		return;
+	}
+	echo '<div class="footer-top">';
+		if($logo) {
+			printf('<div class="logo-footer"><a href="/" class="logo-link">%s</a></div>', wp_get_attachment_image( $logo,'medium'));
+		}
+		if($form) {
+			printf('<div class="newsletter">%s</div>',$form);
+		}
+	echo '</div>';
+}
 
 /**
- * Formulaire newsletter + menus + logo Footer
+ * Grille horaires + coordonnées + liens
  *
  */
 add_action( 'tha_footer_top', 'kasutan_main_footer' );
 function kasutan_main_footer() {
+	$coordonnees=$email=$mentions_legales=$label_horaires=$label_liens='';
+	if(function_exists('get_field')) {
+		$coordonnees=wp_kses_post(get_field('ohm_coordonnees','option'));
+		$email=esc_attr(get_field('ohm_email','option'));
+		$mentions_legales=esc_attr(get_field('ohm_mentions_legales','option')); //champ de type page_id
+		$label_horaires=wp_kses_post(get_field('ohm_label_horaires','option'));
+		$label_liens=wp_kses_post(get_field('ohm_label_liens','option'));
+		
 
-	echo '<a class="backtotop" href="#haut-page"><span class="screen-reader-text">Retour en haut</span>
-		<svg xmlns="http://www.w3.org/2000/svg" width="40" height="23.327" viewBox="0 0 40 23.327"><path d="M106.945,51.7l-2-2a1.26,1.26,0,0,0-1.844,0L87.345,65.444,71.593,49.692a1.26,1.26,0,0,0-1.844,0l-2,2a1.26,1.26,0,0,0,0,1.844L86.423,72.218a1.261,1.261,0,0,0,1.843,0L106.945,53.54a1.263,1.263,0,0,0,0-1.844Z" transform="translate(-67.345 -49.291)" fill="#fff"></path></svg>
-	</a>';
 
-
-	echo '<div class="main-footer"><div class="colonnes-footer">';
+	}
 	
 
-	for($i=1;$i<=3;$i++) {
-		if( has_nav_menu( 'footer-'.$i ) ) {
-			printf('<div class="col-%s col">',$i);
-			wp_nav_menu( array( 'theme_location' => 'footer-'.$i, 'menu_id' => 'footer-'.$i, 'container_class' => 'nav-footer' ) );
+	echo '<div class="main-footer">';
+		//Horaires
+		if(have_rows('ohm_horaires','option')) :
+			echo '<div class="horaires-wrap">';
+			if($label_horaires) printf('<p><strong>%s</strong></p>',$label_horaires);
+			echo '<ul class="horaires">';
+			while(have_rows('ohm_horaires','option')) : the_row();
+				$jour=esc_attr(get_sub_field('jour'));
+				$ouverture=esc_attr(get_sub_field('ouverture'));
+				$fermeture=esc_attr(get_sub_field('fermeture'));
+				$info_supplementaire=esc_attr(get_sub_field('info_supplementaire'));
+				if(!$ouverture) $ouverture='-';
+				if(!$fermeture) $fermeture='-';
+				
+				printf('<li class="horaire"><strong class="jour">%s</strong>',$jour);
+				if($ouverture) printf('<span>%s</span>',$ouverture);
+				if($fermeture) printf('<span>%s</span>',$fermeture);
+				if($info_supplementaire) printf('<span class="info">%s</span>',$info_supplementaire);
 
-			if($i===3 &&  has_nav_menu( 'footer-social')) {
-				wp_nav_menu( array( 'theme_location' => 'footer-social', 'menu_id' => 'footer-social', 'container_class' => 'nav-social' ) );
-			}
+				echo '</li>';
+			endwhile;
+			echo '</ul>';
+			echo '</div>';
+		endif;
+
+
+		//Coordonnées
+		if($coordonnees) {
+			echo '<div class="coordonnees">';
+				printf('<p class="adresse">%s</p>', $coordonnees);
+				if($email) {
+					$email=antispambot( $email);
+					printf('<p><a href="mailto:%s">%s</a>',$email,$email);
+				}
+				if($mentions_legales) {
+					printf('<p><a href="%s">%s</a></p>',
+						get_the_permalink( $mentions_legales),
+						get_the_title( $mentions_legales)
+					);
+				}
+
+			echo '</div>';
+
+		}
+
+
+		//Liens
+		if( has_nav_menu( 'footer-liens') || has_nav_menu('social')) {
+			echo '<div class="liens">';
+				if($label_liens) printf('<strong>%s</strong>',$label_liens);
+				
+				if(has_nav_menu( 'social')) {
+					wp_nav_menu( array( 'theme_location' => 'social', 'menu_id' => 'footer-social', 'container_class' => 'nav-social' ) );
+				}
+				if( has_nav_menu( 'footer-liens') ) {
+					wp_nav_menu( array( 'theme_location' => 'footer-liens', 'menu_id' => 'footer-liens', 'container_class' => 'nav-footer' ) );
+				}
 			echo '</div>';
 		}
-	}
 
-	echo '</div></div>';
+	echo '</div>';
 }
 
 
 /**
-* Copyright et liens légaux
+* Copyright
 *
 */
 add_action( 'tha_footer_bottom', 'kasutan_copyright' );
 function kasutan_copyright() {
-	$mentions_legales=false;
+	$copyright='';
 	if(function_exists('get_field')) {
-		$mentions_legales=esc_attr(get_field('page_mentions_legales','option'));
+		$copyright=wp_kses_post(get_field('ohm_copyright','option'));
 	}
-	echo '<div class="copyright">';
-		//printf('<span class="titre">Copyright &copy; 2022 %s %s</span>',get_option('blogname'), date('Y'));
-		//echo '<span class="sep">-</span>';
-		if( $mentions_legales ) {
-			printf('<a href="%s" class="mentions">%s</a>',get_the_permalink( $mentions_legales),get_the_title($mentions_legales));
-			echo '<span class="sep">-</span>';
-		}
-		echo ('<a class="agence" href="https://banquise.com/" rel="noopener noreferrer" target="_blank">Site internet réalisé par 40 degrés sur la banquise</a>');
-	echo '</div>';
+	if($copyright)
+	printf('<div class="copyright">%s</div>',$copyright);
 }
