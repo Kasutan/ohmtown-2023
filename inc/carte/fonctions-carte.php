12cb8d68392c12bdb39f487efa670ad8239168c9
Kasutan <contact@kasutan.pro>
2023-04-13 17:11:53 +0200

    Elem carte mis en avant fond coloré = option BO

--- inc/carte/fonctions-carte.php
+++ inc/carte/fonctions-carte.php
@@ -4,12 +4,15 @@ if ( ! defined( 'ABSPATH' ) ) exit;
 function kasutan_carte_affiche_li($post_id) {
 	$nom=get_the_title($post_id);
 	$desc=get_the_content($post_id);
-	$prix=false;
+	$prix=$en_avant=false;
 
 	if(function_exists('get_field')) {
 		$prix=wp_kses_post(get_field('prix',$post_id));
+		$en_avant=esc_html(get_field( 'mise_en_avant', $post_id ));
 	}
-	echo '<li class="elem-carte">';
+	$classe=$en_avant ? ' en-avant':'';
+
+	printf('<li class="elem-carte%s">',$classe);
 			printf('<h3 class="nom">%s</h3>',$nom);
 			printf('<div class="desc">%s</div>',$desc);
 			if ($prix) printf('<div class="prix">%s€</div>',$prix);
@@ -24,6 +27,7 @@ function kasutan_set_custom_edit_carte_columns( $columns ) {
 
 
 $columns['prix'] = 'Prix';
+$columns['en_avant'] = 'Mis en avant ?';
 
 
 return $columns;
@@ -41,6 +45,11 @@ switch ( $column ) {
 	
 	case 'prix' :
 		echo esc_html(get_field( 'prix', $post_id )).'€';
+		break;
+
+	case 'en_avant' :
+		$en_avant=esc_html(get_field( 'mise_en_avant', $post_id ));
+		echo $en_avant ? 'oui' : '-';
 		break;
 
 	default:
